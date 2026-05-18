358ada0e189130f5077e7be12776bbd796b4be1e
ShubhamEpsilon0 <snigam10@asu.edu>
2026-05-18 13:33:02 -0700

    Add: fruit-into-baskets - submission-2

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        if not fruits:
+            return 0
+
+        maxFruits = 0
+        start = 0
+        selected_fruits = {fruits[0]: 0}
+
+        for end in range(1, len(fruits)):
+            if fruits[end] not in selected_fruits and len(selected_fruits) == 2:
+                maxFruits = max(maxFruits, end - start)
+                fruit_to_remove = list(set(selected_fruits.keys()) - set([fruits[end - 1]]))[0]
+                start = selected_fruits[fruit_to_remove] + 1
+                del selected_fruits[fruit_to_remove]
+
+            selected_fruits[fruits[end]] = end
+
+        maxFruits = max(maxFruits, len(fruits) - start)
+
+        return maxFruits
+
+        
